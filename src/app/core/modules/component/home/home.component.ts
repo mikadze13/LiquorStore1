42ff5de860fec5b67323189e5087cffa64203e81
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-04-28 16:32:35 +0400

    update

--- src/app/core/modules/component/home/home.component.ts
+++ src/app/core/modules/component/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  
+  
+  backgroundPositionY:any;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY; 
+    const backgroundPosition = -scrollPosition/3  + 'px';
+    this.backgroundPositionY = backgroundPosition;
+  }
+  constructor() { }
+  
+  
+  ngOnInit(): void {
+
+  }
 
 }
