ef2e94b2e48e10c5f8b12589f3ae6e040c6d384d
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-04-28 17:36:39 +0400

    update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  isNavbarHidden: boolean = false;
+  // background:boolean = true
+  isNavbarScrolled!: boolean;
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.scrollY > 0 && window.scrollY < 150) {
+      this.isNavbarScrolled = false
+    } else if (window.scrollY > 149 && window.scrollY < 500) {
+      this.isNavbarHidden = true;
+      this.isNavbarScrolled = true
+    } else {
+      this.isNavbarHidden = false;
+    }
+  }
+  constructor() { }
+  ngOnInit(): void {
+
+  }
 }
